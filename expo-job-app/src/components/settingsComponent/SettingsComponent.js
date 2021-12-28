93b36159722b3a937c1615f75a68e94f9f842393
andressalazard <andres.salazar.d.18@gmail.com>
2021-12-28 11:46:20 -0500

    edit setting component appeareance

--- expo-job-app/src/components/settingsComponent/SettingsComponent.js
+++ expo-job-app/src/components/settingsComponent/SettingsComponent.js
@@ -1,52 +1,53 @@
 import React from "react";
 import styles from "./settingsComponent.module.css";
 
-export default class SettingsComponent extends React.Component{
-  constructor(props){
+export default class SettingsComponent extends React.Component {
+  constructor(props) {
     super(props);
   }
 
-  render(){
-    return(
-    <div className={styles.body}>
-      <form className= {styles.searchForm}>
-        <div className={styles.searchCriteria}>
-          <div className={styles.criteriaOption}>
-            <div className={styles.criteriaHeader}>FILTER</div>
+  render() {
+    return (
+      <div className={styles.body}>
+        <h1>Settings</h1>
+        <form className={styles.searchForm}>
+          <div className={styles.searchCriteria}>
+            <div className={styles.criteriaOption}>
+              <div className={styles.criteriaHeader}>FILTER YOUR SEARCH</div>
               <div className={styles.childOption}>
-                <input type="checkbox" id="fTimeFilter" value= "fTime"/>
-                <label for= "fTimeFilter">Full Time</label>
+                <input type="checkbox" id="fTimeFilter" value="fTime" />
+                <label for="fTimeFilter">Full Time</label>
               </div>
-          </div>
+            </div>
 
-          <div className={styles.criteraOption}>
-            <div className={styles.criteriaHeader}>SEARCHBAR</div>
-              <input type="text" placeholder="City, state, zip code or country"/>
-          </div>
+            <div className={styles.criteriaOption}>
+              <div className={styles.criteriaHeader}>SEARCH A LOCATION</div>
+              <input type="text" placeholder="City, state, zip code or country" />
+            </div>
 
-          <div className={styles.criteriaOption}>
-            <div className={styles.criteriaHeader}>LOCATIONS</div>
+            <div className={styles.criteriaOption}>
+              <div className={styles.criteriaHeader}>POPULAR LOCATIONS</div>
               <div className={styles.childOption}>
-                <input type="radio" id="1" name="option1" value="option1" checked/>
+                <input type="radio" id="1" name="option1" value="option1" checked />
                 <label for="1">LONDON</label>
               </div>
               <div className={styles.childOption}>
-                <input type="radio" id="2" name="option2" value="option2"/>
+                <input type="radio" id="2" name="option2" value="option2" />
                 <label for="2">LONDON</label>
               </div>
               <div className={styles.childOption}>
-                <input type="radio" id="3" name="option3" value="option3"/>
+                <input type="radio" id="3" name="option3" value="option3" />
                 <label for="3">LONDON</label>
               </div>
               <div className={styles.childOption}>
-                <input type="radio" id="4" name="option4" value="option4"/>
+                <input type="radio" id="4" name="option4" value="option4" />
                 <label for="4">LONDON</label>
               </div>
+            </div>
           </div>
-
-        </div>
-        <button>GO SEARCH!</button>
-      </form>            
-    </div>)
+          <button>GO SEARCH</button>
+        </form>
+      </div>
+    );
   }
 }
